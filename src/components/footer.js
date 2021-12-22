3d415af458fe09db22702cd80beb8e3fc16d7181
My Name <my-name@chromium.org>
2021-12-22 23:22:33 +0100

    Initial commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      Technische Umsetzung durch Rene Langenberger
+      Technische Umsetzung: Rene Langenberger
     </div>
   </Container>
 )
