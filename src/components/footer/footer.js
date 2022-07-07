e544f79338aee363368955075bc05486f62359e4
My Name <my-name@chromium.org>
2022-07-07 22:37:52 +0200

    add mailchimp

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,14 +2,13 @@ import React from 'react'
 
 import * as styles from './footer.module.css'
 import { Link } from 'gatsby'
+import Newsletter from '../newsletter/newsletter'
 
 const Footer = () => (
   <>
     <div className={styles.newsletter}>
       Newsletter
-      <div className={styles.action}>
-        <input />
-      </div>
+      <Newsletter></Newsletter>
     </div>
     <div className={styles.footer}>
       <Link to='/impressum/' activeClassName='active' className={styles.footerItem}>
