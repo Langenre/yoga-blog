4d1800a27b5cc1c709686239d34274b7437df25b
lvharten <90825143+lvharten@users.noreply.github.com>
2022-07-07 21:00:33 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,11 +17,23 @@ export default function Component (props) {
                     className={styles.body}
                     >
                         <p>Namasté</p>
-                        <p>Ich bin Laura und habe mich auf die Reise zur Yogalehrerin begeben. Voraussichtlich werde ich im Januar meine Ausbildung (BYV) im Seminarhaus Bad Meinberg abschließen.</p>
-                        <p>Für mich ist Yoga [...]</p>
-                        <p>Die Faszination für [...] lasse ich in meine persönliche Yogaübungspraxis einfließen.</p>
-                        <p>Mein Leitgedanke ist [...]</p>
-                        <p>Meinen Schülern möchte ich [...] mitgeben.</p>
+                        <p> Schön, dass du da bist!</p>
+                        
+                        <p>Mein Name ist Laura und ich praktiziere seit 2018 regelmäßig Yoga. Meine Yogareise begann auf Youtube, als ich nach einer Trennung mit Videos von Mady Morrison meinem Körper etwas Gutes tun wollte </p>
+                        <p> Schnell habe ich gemerkt, dass Yoga aber weit mehr kann, als mich gelenkiger zu machen. Je regelmäßiger ich praktizierter, desto mehr änderte sich für mich. </p>
+                        <p> Und das nicht nur körperlich, sondern auch geistig. Ich lernte, mich zu spüren und meine Emotionen wahrzunehmen. Achtsamer mit mir zu sein. Mich zu akzeptieren, so wie ich bin. Alte Spannungen loszulassen. Yoga war für mich der erste Schritt auf einer Reise zu innerer Heilung. </p>
+                        <p> Trotzdem liebe ich es auch, mich auf der Matte herauszufordern. Meinen Körper jedes Mal neu kennen zu lernen. Und zu beobachten, wie ich mich über die Jahre auf der Matte entwickle. </p>
+                        <p> 2022 schloss ich schließlich meine Ausbildung zur Hatha-Yoga Lehrerin bei Yoga Vidya ab. So kann ich das, was ich auf der Matte erleben durfte, auch andere erleben lassen. </p>
+                         <p> Ich glaube nämlich, dass wir viel mehr Yogis in dieser Welt brauchen und Achtsamkeit nicht nur zu innerem sondern auch äußerem Frieden führen kann. </p>   
+                           <p> Darum möchte ich  auch dir die Vielseitigkeit und Heilsamkeit von Yoga zeigen. </p>
+                        <P> In meinen Stunden lasse ich darum immer wieder kleine philosophische Impulse einfließen. Außerdem lege ich viel Wert auf Ausgeglichenheit zwischen Bewegung und Entspannung. </p>
+                        <P> So möchte ich Yoga möglichst ganzheitlich vermitteln.</p>
+
+                        <p> Ich freue mich, wenn ich dich bald in einer meiner Stunden begrüßen darf. </p>
+                        <P> Schaue gerne in mein Kursangebot oder kontaktiere mich jederzeit, wenn du Fragen hast.</p>
+                        <p> Ich freue mich auf dich! </p>
+                           
+    ´                    
                     </div>
                 </div>
             </div>
